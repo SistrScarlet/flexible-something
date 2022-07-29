2c75378df3b7d2600a8ff483e912c03a65403603
SistrScarlet <game.sistr@gmail.com>
2022-07-29 22:13:15 +0900

    raidの仕様を変更

--- common/src/main/java/net/sistr/flexiblesomething/mixin/MixinPlayerEntity.java
+++ common/src/main/java/net/sistr/flexiblesomething/mixin/MixinPlayerEntity.java
@@ -1,78 +1,78 @@
 package net.sistr.flexiblesomething.mixin;
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.StackReference;
-import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.sistr.flexiblesomething.entity.GreedChunkEntity;
+import net.sistr.flexiblesomething.entity.GreedRaid;
 import net.sistr.flexiblesomething.util.Raidable;
-import org.apache.commons.compress.utils.Lists;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
-
 @Mixin(PlayerEntity.class)
 public abstract class MixinPlayerEntity extends LivingEntity implements Raidable {
-    private final List<Entity> raidingGreed = Lists.newArrayList();
+    private GreedRaid greedRaid;
 
     @Shadow
     public abstract StackReference getStackReference(int mappedIndex);
-
-    private boolean isRaidedByGreed;
 
     protected MixinPlayerEntity(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
 
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(World world, BlockPos pos, float yaw, GameProfile profile, CallbackInfo ci) {
+        greedRaid = new GreedRaid(world, (PlayerEntity) (Object) this);
+    }
+
     @Inject(method = "writeCustomDataToNbt", at = @At("RETURN"))
     private void onWrite(NbtCompound nbt, CallbackInfo ci) {
-        nbt.putBoolean("isRaidedByGreed", isRaidedByGreed);
     }
 
     @Inject(method = "readCustomDataFromNbt", at = @At("RETURN"))
     private void onRead(NbtCompound nbt, CallbackInfo ci) {
-        isRaidedByGreed = nbt.getBoolean("isRaidedByGreed");
+    }
+
+    @Override
+    public void onKilledOther(ServerWorld world, LivingEntity other) {
+        super.onKilledOther(world, other);
+        greedRaid.killRaidEntity(other);
     }
 
     @Inject(method = "tick", at = @At("RETURN"))
     private void onTick(CallbackInfo ci) {
-        this.isRaidedByGreed = this.getOffHandStack().getItem() == Items.CLOCK;
-        if (isRaidedByGreed) {
-            raidingGreed.removeIf(Entity::isRemoved);
-            if (raidingGreed.size() < 300) {
-                var rand = this.getRandom();
-                int x = (int) (this.getX() + (rand.nextFloat() * 2 - 1) * 64);
-                int y = this.world.getHeight();
-                int z = (int) (this.getZ() + (rand.nextFloat() * 2 - 1) * 64);
-                var greedChunk = new GreedChunkEntity(this.world);
-                greedChunk.setTargetPlayer((PlayerEntity) (Object) this);
-                greedChunk.setPosition(x, y, z);
-                this.world.spawnEntity(greedChunk);
-                this.raidingGreed.add(greedChunk);
-            }
+        if (this.world.isClient) {
+            return;
         }
+        greedRaid.tick();
     }
 
     @Override
     public void addRaidEntity(Entity entity) {
-        this.raidingGreed.add(entity);
+        this.greedRaid.addRaidEntity(entity);
     }
 
     @Override
     public void removeRaidEntity(Entity entity) {
-        this.raidingGreed.remove(entity);
+        this.greedRaid.removeRaidEntity(entity);
     }
 
     @Override
     public boolean isRaiding() {
-        return this.isRaidedByGreed;
+        return greedRaid.isRaiding();
+    }
+
+    @Override
+    public int getRaidKills() {
+        return greedRaid.getRaidKills();
     }
 }
